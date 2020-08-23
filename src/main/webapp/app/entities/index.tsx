dd753d305b2193291eac0aea2809aa4aab79b1d2
nicopatronelli <nicopatronelli@gmail.com>
2020-08-23 01:44:34 -0300

    Regular users can't access to Permissions & UserWithPerms entities added

--- src/main/webapp/app/entities/index.tsx
+++ src/main/webapp/app/entities/index.tsx
@@ -9,13 +9,16 @@ import UserWithPerms from './user-with-perms';
 import Permissions from './permissions';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
+import { AUTHORITIES } from 'app/config/constants';
+import PrivateRoute from 'app/shared/auth/private-route';
+
 const Routes = ({ match }) => (
   <div>
     <Switch>
       {/* prettier-ignore */}
-      <ErrorBoundaryRoute path={`${match.url}product`} component={Product} />
-      <ErrorBoundaryRoute path={`${match.url}user-with-perms`} component={UserWithPerms} />
-      <ErrorBoundaryRoute path={`${match.url}permissions`} component={Permissions} />
+      <PrivateRoute path={`${match.url}product`} component={Product} hasAnyAuthorities={[AUTHORITIES.USER]} />
+      <PrivateRoute path={`${match.url}user-with-perms`} component={UserWithPerms} hasAnyAuthorities={[AUTHORITIES.ADMIN]} />
+      <PrivateRoute path={`${match.url}permissions`} component={Permissions} hasAnyAuthorities={[AUTHORITIES.ADMIN]} />
       {/* jhipster-needle-add-route-path - JHipster will add routes here */}
     </Switch>
   </div>
--- src/main/webapp/app/shared/layout/menus/entities.tsx
+++ src/main/webapp/app/shared/layout/menus/entities.tsx
@@ -6,7 +6,7 @@ import { Translate, translate } from 'react-jhipster';
 import { NavLink as Link } from 'react-router-dom';
 import { NavDropdown } from './menu-components';
 
-export const EntitiesMenu = props => (
+export const EntitiesMenu = (props: {isAdmin: boolean}) => (
   <NavDropdown
     icon="th-list"
     name={translate('global.menu.entities.main')}
@@ -16,12 +16,16 @@ export const EntitiesMenu = props => (
     <MenuItem icon="asterisk" to="/product">
       <Translate contentKey="global.menu.entities.product" />
     </MenuItem>
-    <MenuItem icon="asterisk" to="/user-with-perms">
-      <Translate contentKey="global.menu.entities.userWithPerms" />
-    </MenuItem>
-    <MenuItem icon="asterisk" to="/permissions">
-      <Translate contentKey="global.menu.entities.permissions" />
-    </MenuItem>
+    {props.isAdmin && 
+    <span>
+      <MenuItem icon="asterisk" to="/user-with-perms">
+        <Translate contentKey="global.menu.entities.userWithPerms" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/permissions">
+        <Translate contentKey="global.menu.entities.permissions" />
+      </MenuItem> 
+      </span>
+      }
     {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
   </NavDropdown>
 );
