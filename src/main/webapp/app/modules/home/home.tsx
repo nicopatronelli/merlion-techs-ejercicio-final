50e497b5093ee2af108865bfa9241b8f4547f96e
nicopatronelli <nicopatronelli@gmail.com>
2020-08-26 15:34:31 -0300

    Home updated

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
 import { Container, Row, Col, Alert } from 'reactstrap';
-import StatsChartsTabs from '../statistical-charts/StartsChartsTabs';
+import StatsChartsTabs from '../statistical-charts/StatsChartsTabs';
 import { hasAnyAuthority } from 'app/shared/auth/private-route';
 import { AUTHORITIES } from 'app/config/constants';
 
@@ -15,40 +15,35 @@ export const Home = (props: IHomeProp) => {
 
   return (
     <Container>
-    <Row>
-      <Col>
-        <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-        </h2>
-        <p className="lead">
-          <Translate contentKey="home.subtitle">This is your homepage</Translate>
-        </p>
-        {account && account.login ? (
-          null
-        ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
+      <Row>
+        <Col>
+          <h2>Estadísticas del negocio</h2>
+          {account && account.login ? (
+            null
+          ) : (
+              <div>
+                <Alert color="warning">
+                  <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
+                  <Link to="/login" className="alert-link">
+                    <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
+                  </Link>
+                  <Translate contentKey="global.messages.info.authenticated.suffix">
+                    , you can try the default accounts:
                 <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
                 <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
               </Translate>
-            </Alert>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+                </Alert>
+                <Alert color="warning">
+                  <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
               <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
-      {isAdmin ? <StatsChartsTabs /> : null}
-      </Col>
-    </Row>
+                    <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
+                  </Link>
+                </Alert>
+              </div>
+            )}
+          {isAdmin ? <StatsChartsTabs /> : null}
+        </Col>
+      </Row>
     </Container>
   );
 };
